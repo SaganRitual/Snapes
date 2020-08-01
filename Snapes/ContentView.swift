8c7c06d26158a9c61497a5f6df6a6590ba57e1ab
Rob Bishop <c.sagan.ritual@gmail.com>
2020-08-01 10:16:07 -0700

    Three rectangles

--- Snapes/ContentView.swift
+++ Snapes/ContentView.swift
@@ -8,8 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var textUnitSize = CGSize.zero
+
+    let scale: CGSize
+    let xScale = CGFloat(450)
+    let yScale = CGFloat(300)
+
+    init() { scale = CGSize(width: xScale, height: yScale) }
+
     var body: some View {
-        Text("Hello, world!").padding()
+        ZStack {
+            Rectangle()
+                .frame(width: 1.1 * xScale, height: 1.1 * yScale)
+                .background(Color(white: 0.9))
+
+            Rectangle()
+                .frame(width: 0.1 * xScale, height: 1.0 * yScale)
+                .background(Color(white: 0.5))
+                .offset(x: -0.5 * xScale, y: -0.05 * yScale)
+
+            Rectangle()
+                .frame(width: 1.0 * xScale, height: 0.1 * yScale)
+                .background(Color(white: 0.5))
+                .offset(x: 0.05 * xScale, y: 0.5 * yScale)
+        }
     }
 }
 
