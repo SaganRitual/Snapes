91f96128bb907b6baa9e5e68a60ccb8b0739d67c
Rob Bishop <c.sagan.ritual@gmail.com>
2020-08-01 09:57:01 -0700

    Initial Commit

---
+++ Snapes/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Snapes
+//
+//  Created by Rob Bishop on 8/1/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
